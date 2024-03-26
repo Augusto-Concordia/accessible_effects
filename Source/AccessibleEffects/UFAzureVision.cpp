caa38331ded74d15d8e86e4a8e1a744d9464b173
Augusto Mota Pinheiro <augustomp55@gmail.com>
2024-03-25 20:39:44 -0400

    feat: completed SeeingAI!

--- Source/AccessibleEffects/UFAzureVision.cpp
+++ Source/AccessibleEffects/UFAzureVision.cpp
@@ -7,14 +7,15 @@
 #include "Http.h"
 #include "UFUtilities.h"
 
+#define CHECK_BIT(var, pos) (((var)>>(pos)) & 1)
 
 void UFAzureVision::GetImageDenseCaptions(const FAzureVisionSettings& Settings,
-                                         const TArray<uint8>& ImageData,
-                                         const FOnSuccess OnSuccess,
-                                         const FOnFailure OnFailure)
+                                          const TArray<uint8>& ImageData,
+                                          const FOnSuccess& OnSuccess,
+                                          const FOnFailure& OnFailure)
 {
 	FHttpModule& HttpModule = FHttpModule::Get();
-	TSharedRef<IHttpRequest> Request = HttpModule.CreateRequest();
+	const TSharedRef<IHttpRequest> Request = HttpModule.CreateRequest();
 
 	// Set up the request
 	Request->SetVerb(TEXT("POST"));
@@ -44,7 +45,7 @@ void UFAzureVision::GetImageDenseCaptions(const FAzureVisionSettings& Settings,
 				return;
 			}
 
-			ProcessAzureVisionContent(Response->GetContentAsString(), &OnSuccess, &OnFailure);
+			ProcessAzureVisionContent(Response->GetContentAsString(), OnSuccess, OnFailure);
 		});
 
 	// Submit the request
@@ -52,8 +53,8 @@ void UFAzureVision::GetImageDenseCaptions(const FAzureVisionSettings& Settings,
 }
 
 void UFAzureVision::ProcessAzureVisionContent(const FString& Content,
-                                              const FOnSuccess* OnSuccess,
-                                              const FOnFailure* OnFailure)
+                                              const FOnSuccess& OnSuccess,
+                                              const FOnFailure& OnFailure)
 {
 	UE_LOG(LogTemp, Display, TEXT("Received: %s"), *Content);
 
@@ -63,29 +64,48 @@ void UFAzureVision::ProcessAzureVisionContent(const FString& Content,
 
 	if (!FJsonSerializer::Deserialize(Reader, JsonObject))
 	{
-		auto _ = OnFailure->ExecuteIfBound(TEXT("Failed to parse JSON response"));
+		auto _ = OnFailure.ExecuteIfBound(TEXT("Failed to parse JSON response"));
+		return;
+	}
+
+	const TSharedPtr<FJsonObject>* Metadata;
+	if (!JsonObject->TryGetObjectField(TEXT("metadata"), Metadata))
+	{
+		auto _ = OnFailure.ExecuteIfBound(TEXT("Failed to find metadata in denseCaptionsResult"));
+		return;
+	}
+
+	int32 ImageWidth;
+	if (!(*Metadata)->TryGetNumberField(TEXT("width"), ImageWidth))
+	{
+		auto _ = OnFailure.ExecuteIfBound(TEXT("Failed to find width in metadata"));
+		return;
+	}
+
+	int32 ImageHeight;
+	if (!(*Metadata)->TryGetNumberField(TEXT("height"), ImageHeight))
+	{
+		auto _ = OnFailure.ExecuteIfBound(TEXT("Failed to find height in metadata"));
 		return;
 	}
 
 	const TSharedPtr<FJsonObject>* DenseCaptionsResult;
-
 	if (!JsonObject->TryGetObjectField(TEXT("denseCaptionsResult"), DenseCaptionsResult))
 	{
-		auto _ = OnFailure->ExecuteIfBound(TEXT("Failed to find denseCaptionsResult in JSON response"));
+		auto _ = OnFailure.ExecuteIfBound(TEXT("Failed to find denseCaptionsResult in JSON response"));
 		return;
 	}
 
 	const TArray<TSharedPtr<FJsonValue>>* Values;
-
 	if (!(*DenseCaptionsResult)->TryGetArrayField(TEXT("values"), Values))
 	{
-		auto _ = OnFailure->ExecuteIfBound(TEXT("Failed to find values in denseCaptionsResult"));
+		auto _ = OnFailure.ExecuteIfBound(TEXT("Failed to find values in denseCaptionsResult"));
 		return;
 	}
 
 	UE_LOG(LogTemp, Display, TEXT("Found %d dense captions in response"), Values->Num());
 
-	for (const auto &JsonCaption : *Values)
+	for (const auto& JsonCaption : *Values)
 	{
 		const TSharedPtr<FJsonObject> JsonCaptionObject = JsonCaption->AsObject();
 
@@ -94,13 +114,82 @@ void UFAzureVision::ProcessAzureVisionContent(const FString& Content,
 		Caption.Confidence = JsonCaptionObject->GetNumberField("confidence");
 
 		const TSharedPtr<FJsonObject> JsonBoundingBoxObject = JsonCaptionObject->GetObjectField("boundingBox");
-		Caption.Position = FVector2D(JsonBoundingBoxObject->GetNumberField("x"), JsonBoundingBoxObject->GetNumberField("y"));
-		Caption.Size = FVector2D(JsonBoundingBoxObject->GetNumberField("w"), JsonBoundingBoxObject->GetNumberField("h"));
+		Caption.Position = FVector2D(JsonBoundingBoxObject->GetNumberField("x"),
+		                             JsonBoundingBoxObject->GetNumberField("y"));
+		Caption.Size = FVector2D(JsonBoundingBoxObject->GetNumberField("w"),
+		                         JsonBoundingBoxObject->GetNumberField("h"));
 
 		DenseCaptions.Add(Caption);
 	}
 
 	DenseCaptions.Sort();
+	DenseCaptions = DenseCaptions.FilterByPredicate([](const FDenseCaption& Caption)
+	{
+		return Caption.Confidence > 0.75;
+	});
 
-	//auto _ = OnSuccess.ExecuteIfBound(DenseCaptions);
+	FString FullCaptionText;
+	uint8 Flags = 0;
+
+	for (int i = 0; i < DenseCaptions.Num(); ++i)
+	{
+		const auto& [Text, Confidence, Position, Size] = DenseCaptions[i];
+
+		// If all flags are set, break
+		if (!(Flags ^ 0b11111))
+		{
+			break;
+		}
+
+		if (DenseCaptions.Num() == 0)
+		{
+			FullCaptionText = FullCaptionText.Append("I'm sorry, I could not find anything in the image.");
+			continue;
+		}
+
+		// top left
+		if (!CHECK_BIT(Flags, 0) &&
+			Position.Component(0) + Size.Component(0) / 2 < ImageWidth / 2 &&
+			Position.Component(1) + Size.Component(1) / 2 < ImageHeight / 2)
+		{
+			Flags |= 0b00001;
+			FullCaptionText = FullCaptionText.Append(Text).Append(" on the top left corner. ");
+		}
+		// top right
+		else if (!CHECK_BIT(Flags, 1) &&
+			Position.Component(0) + Size.Component(0) / 2 >= ImageWidth / 2 &&
+			Position.Component(1) + Size.Component(1) < ImageHeight / 2)
+		{
+			Flags |= 0b00010;
+			FullCaptionText = FullCaptionText.Append(Text).Append(" on the top right corner. ");
+		}
+		// bottom left
+		else if (!CHECK_BIT(Flags, 2) &&
+			Position.Component(0) + Size.Component(0) / 2 < ImageWidth / 2 &&
+			Position.Component(1) + Size.Component(1) >= ImageHeight / 2)
+		{
+			Flags |= 0b00100;
+			FullCaptionText = FullCaptionText.Append(Text).Append(" on the bottom left corner. ");
+		}
+		// bottom right
+		else if (!CHECK_BIT(Flags, 3) &&
+			Position.Component(0) + Size.Component(0) / 2 >= ImageWidth / 2 &&
+			Position.Component(1) + Size.Component(1) >= ImageHeight / 2)
+		{
+			Flags |= 0b01000;
+			FullCaptionText = FullCaptionText.Append(Text).Append(" on the bottom right corner. ");
+		}
+		// center
+		else if (!CHECK_BIT(Flags, 4) &&
+			Position.Component(0) + Size.Component(0) / 2 >= ImageWidth / 4 &&
+			Position.Component(0) + Size.Component(0) / 2 <= 3 * ImageWidth / 4 &&
+			Position.Component(1) + Size.Component(1) / 2 >= ImageHeight / 4 &&
+			Position.Component(1) + Size.Component(1) / 2 <= 3 * ImageHeight / 4)
+		{
+			Flags |= 0b10000;
+			FullCaptionText = FullCaptionText.Append(Text).Append(" in the center. ");
+		}
+	}
+
+	auto _ = OnSuccess.ExecuteIfBound(FullCaptionText);
 }
--- Source/AccessibleEffects/UFAzureVision.h
+++ Source/AccessibleEffects/UFAzureVision.h
@@ -4,15 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "FAzureVisionSettings.h"
+#include "FDenseCaptions.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "UFAzureVision.generated.h"
 
-DECLARE_DYNAMIC_DELEGATE_OneParam(FOnSuccess, FString, Captions);
-DECLARE_DYNAMIC_DELEGATE_OneParam(FOnFailure, FString, Msg);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FOnSuccess, FString, Caption);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FOnFailure, FString, ErrorMessage);
 
-/**
- * 
- */
 UCLASS()
 class ACCESSIBLEEFFECTS_API UFAzureVision : public UBlueprintFunctionLibrary
 {
@@ -20,9 +18,9 @@ class ACCESSIBLEEFFECTS_API UFAzureVision : public UBlueprintFunctionLibrary
 
 public:
 	UFUNCTION(BlueprintCallable, Category = "Azure Vision")
-	static void GetImageDenseCaptions(const FAzureVisionSettings& Settings, const TArray<uint8>& ImageData, const FOnSuccess OnSuccess, const FOnFailure
+	static void GetImageDenseCaptions(const FAzureVisionSettings& Settings, const TArray<uint8>& ImageData, const FOnSuccess& OnSuccess, const FOnFailure&
 	                                 OnFailure);
 
 private:
-	static void ProcessAzureVisionContent(const FString& Content, const FOnSuccess* OnSuccess, const FOnFailure* OnFailure);
+	static void ProcessAzureVisionContent(const FString& Content, const FOnSuccess& OnSuccess, const FOnFailure& OnFailure);
 };
