2510d1e5ecd45163751b9a76977b88e724ce0e6c
Augusto Mota Pinheiro <augustomp55@gmail.com>
2024-03-27 15:19:03 -0400

    feat: completed demo videos

--- Content/BetterVRSource/BetterVR/HostProject/Intermediate/Build/Win64/x64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp17.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.39.33523
-#include "D:/Projects/Concordia/comp_475/Project/Content/BetterVRSource/BetterVR/HostProject/Intermediate/Build/Win64/x64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp17.h"
